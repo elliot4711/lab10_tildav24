ffce2806a9bcc25d643e280f01f5dbbfbe33e87d
Elliot Stjernqvist <elliot@n152-p241.eduroam.kth.se>
2024-04-08 17:04:13 +0200

    Started weights part

--- formula_check.py
+++ formula_check.py
@@ -9,6 +9,131 @@ periodic_table = ['H', 'He', 'Li', 'Be', 'B', 'C', 'N', 'O', 'F', 'Ne', 'Na', 'M
 'Pt', 'Au', 'Hg', 'Tl', 'Pb', 'Bi', 'Po', 'At', 'Rn', 'Fr', 'Ra', 'Ac', 'Th', 'Pa', 'U', 'Np', 'Pu',
 'Am', 'Cm', 'Bk', 'Cf', 'Es', 'Fm', 'Md', 'No', 'Lr', 'Rf', 'Db', 'Sg', 'Bh', 'Hs', 'Mt', 'Ds', 'Rg', 'Cn', 'Fl', "Lv"]
 
+
+def create_atomdict():
+    """Creates and returns dictionary of atom weights"""
+    
+    atomdata = "H 1.00794;\
+    He 4.002602;\
+    Li 6.941;\
+    Be 9.012182;\
+    B 10.811;\
+    C 12.0107;\
+    N 14.0067;\
+    O 15.9994;\
+    F 18.9984032;\
+    Ne 20.1797;\
+    Na 22.98976928;\
+    Mg 24.3050;\
+    Al 26.9815386;\
+    Si 28.0855;\
+    P 30.973762;\
+    S 32.065;\
+    Cl 35.453;\
+    K 39.0983;\
+    Ar 39.948;\
+    Ca 40.078;\
+    Sc 44.955912;\
+    Ti 47.867;\
+    V 50.9415;\
+    Cr 51.9961;\
+    Mn 54.938045;\
+    Fe 55.845;\
+    Ni 58.6934;\
+    Co 58.933195;\
+    Cu 63.546;\
+    Zn 65.38;\
+    Ga 69.723;\
+    Ge 72.64;\
+    As 74.92160;\
+    Se 78.96;\
+    Br 79.904;\
+    Kr 83.798;\
+    Rb 85.4678;\
+    Sr 87.62;\
+    Y 88.90585;\
+    Zr 91.224;\
+    Nb 92.90638;\
+    Mo 95.96;\
+    Tc 98;\
+    Ru 101.07;\
+    Rh 102.90550;\
+    Pd 106.42;\
+    Ag 107.8682;\
+    Cd 112.411;\
+    In 114.818;\
+    Sn 118.710;\
+    Sb 121.760;\
+    I 126.90447;\
+    Te 127.60;\
+    Xe 131.293;\
+    Cs 132.9054519;\
+    Ba 137.327;\
+    La 138.90547;\
+    Ce 140.116;\
+    Pr 140.90765;\
+    Nd 144.242;\
+    Pm 145;\
+    Sm 150.36;\
+    Eu 151.964;\
+    Gd 157.25;\
+    Tb 158.92535;\
+    Dy 162.500;\
+    Ho 164.93032;\
+    Er 167.259;\
+    Tm 168.93421;\
+    Yb 173.054;\
+    Lu 174.9668;\
+    Hf 178.49;\
+    Ta 180.94788;\
+    W 183.84;\
+    Re 186.207;\
+    Os 190.23;\
+    Ir 192.217;\
+    Pt 195.084;\
+    Au 196.966569;\
+    Hg 200.59;\
+    Tl 204.3833;\
+    Pb 207.2;\
+    Bi 208.98040;\
+    Po 209;\
+    At 210;\
+    Rn 222;\
+    Fr 223;\
+    Ra 226;\
+    Ac 227;\
+    Pa 231.03588;\
+    Th 232.03806;\
+    Np 237;\
+    U 238.02891;\
+    Am 243;\
+    Pu 244;\
+    Cm 247;\
+    Bk 247;\
+    Cf 251;\
+    Es 252;\
+    Fm 257;\
+    Md 258;\
+    No 259;\
+    Lr 262;\
+    Rf 265;\
+    Db 268;\
+    Hs 270;\
+    Sg 271;\
+    Bh 272;\
+    Mt 276;\
+    Rg 280;\
+    Ds 281;\
+    Cn 285"
+
+    atom_list = atomdata.split(";")
+    weight_dict = {}
+    for atom in atom_list:
+        name, weight = atom.split()
+        weight_dict[name] = float(weight)
+    
+    return weight_dict
+
 class Ruta:
     def __init__(self, atom="( )", num=1):
         self.atom = atom
@@ -42,8 +167,8 @@ def formula():
                 while not que.isEmpty():
                     mol = ismolecule(que)
                 print("Formeln är syntaktiskt korrekt")
-                print(mol.atom)
-                print(mol.num)
+                atom_dict = create_atomdict()
+                print(f"Molecule weight is: {weight(mol, atom_dict)}")
                 mg = Molgrafik()
                 mg.show(mol)
             except Syntaxfel as err:
@@ -214,6 +339,21 @@ def get_word(que):
             l = que.dequeue()
             word += l
     return word
+
+def weight(mol, atom_dict):
+
+    if mol.atom.isalpha() and mol.next != None:
+        mol_weight = atom_dict(mol.atom) * mol.num + weight(mol.next, atom_dict)
+
+    elif mol.atom == "( )" and mol.next != None: 
+        mol_weight = weight(mol.down) * mol.num + weight(mol.down, atom_dict)
+
+    elif mol.atom.isalpha():
+        mol_weight = atom_dict(mol.atom) * mol.num
+    
+    return mol_weight
+
+
 
 if __name__ == '__main__':
     formula()
